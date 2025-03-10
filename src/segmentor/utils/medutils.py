2525faea2d871f7d9ad1ac64caf087d2a429fcf9
mkrastev <matey_krastev2@hotmail.com>
2025-03-10 14:56:08 +0100

    update utils

--- src/segmentor/utils/medutils.py
+++ src/segmentor/utils/medutils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 import nibabel as nib
 from nibabel.processing import resample_from_to
@@ -7,10 +8,20 @@ import numpy.typing as npt
 
 
 def load_nifti(img_path: str | Path) -> npt.NDArray:
+    """Loads a NIfTI image from the given path and returns it as a NumPy array."""
     return np.asarray(nib.load(img_path).dataobj)
 
 
 def save_nifti(data: npt.NDArray, filename: str | Path, other: str | Path = None):
+    """
+    Saves a NumPy array as a NIfTI image.
+
+    Args:
+        data (npt.NDArray): The NumPy array to save.
+        filename (str | Path): The filename to save the image to.
+        other (str | Path, optional): Another NIfTI image to copy the header from. Defaults to None.
+                                        If None, the header from the image to be saved is used.
+    """
     if other is None:
         other = nib.load(filename)
     else:
@@ -20,36 +31,86 @@ def save_nifti(data: npt.NDArray, filename: str | Path, other: str | Path = None
 
 
 def normalize_ct(
-    nii,
-    quantiles: tuple[float, float] = (0.0005, 0.9995),
-    window: tuple[float, float] = (50, 400),
-):
-    nii = np.where(
-        (nii > np.quantile(nii, quantiles[0])) & (nii < (np.quantile(nii, quantiles[1]))),
-        nii,
-        0,
-    )
-    window_c, window_w = window
-    nii = np.where((nii >= window_c - window_w / 2) & (nii <= window_c + window_w / 2), nii, 0)
-    # nii = nii / np.linalg.norm(nii, axis=-1, keepdims=True)
+    nii: npt.NDArray,
+    quantiles: Optional[tuple[float, float]] = (0.0005, 0.9995),
+    window: Optional[tuple[float, float]] = (50, 400),
+) -> npt.NDArray:
+    """
+    Normalizes a CT scan image.
+
+    Args:
+        nii (npt.NDArray): The CT scan image as a NumPy array.
+        quantiles (tuple[float, float], optional): The quantiles to use for clipping. Defaults to (0.0005, 0.9995).
+        window (tuple[float, float], optional): The window to use for normalization. Defaults to (50, 400).
+
+    Returns:
+        npt.NDArray: The normalized CT scan image.
+    """
+    # Clip values outside the specified quantiles to 0
+    if quantiles:
+        nii = np.clip(nii, *np.quantile(nii, quantiles))
+    if window:
+        window_c, window_w = window
+        low = window_c - window_w / 2
+        high = window_c + window_w / 2
+        # Clip to window
+        nii = np.clip(nii, low, high)
+        # Normalize to [0, 1]
+        nii = (nii - low) / (high - low)
+    else:
+        nii = (nii - np.min(nii)) / (np.max(nii) - np.min(nii))
     return nii
 
-
-def load_and_normalize_nifti(filename, quantiles=(0.0005, 0.9995), window=(50, 400)):
+def load_and_normalize_nifti(
+    filename: str | Path,
+    quantiles: Optional[tuple[float, float]] = (0.0005, 0.9995),
+    window: Optional[tuple[float, float]] = (50, 400),
+) -> npt.NDArray:
+    """Loads a NIfTI image from the given path, normalizes it, and returns it as a NumPy array."""
     nii = nib.load(filename)
     nii = np.asarray(nii.dataobj)
     nii = normalize_ct(nii, quantiles, window)
     return nii
 
 
-def load_and_resample_nifti(filename: str, factor=0.5, order=0, normalize=False):
+def load_and_resample_nifti(
+    filename: str, factor: float = 0.5, order: int = 0, normalize: bool = False, **kwargs
+) -> nib.Nifti1Image:
+    """
+    Loads a NIfTI image from the given path, resamples it, and returns it as a NIfTI image.
+
+    Args:
+        filename (str): The path to the NIfTI image.
+        factor (float, optional): The resampling factor. Defaults to 0.5.
+        order (int, optional): The order of interpolation. Defaults to 0.
+        normalize (bool, optional): Whether to normalize the image before resampling. Defaults to False.
+        **kwargs: Additional keyword arguments to pass to the normalization function.
+    Returns:
+        nib.Nifti1Image: The resampled NIfTI image.
+    """
     # Load the image
     image = nib.load(filename)
+    image_data = np.asarray(image.dataobj)
+
     if normalize:
-        image.dataobj = normalize_ct(image.dataobj)
-    target_shape = np.array(image.affine) * factor
-    # Create a new affine matrix by copying the original and scaling the rotation/scaling part.
-    # This assumes the original affine encodes isotropic voxel sizes.
-    new_affine = image.affine.copy()
-    new_affine[:3, :3] *= 1 / factor
-    return resample_from_to(image, (target_shape, new_affine), order=order)
+        image_data = normalize_ct(image_data, **kwargs)
+
+    # Resample the image data
+    # Original image affine
+    affine = image.affine
+
+    # Original image shape
+    shape = np.array(image_data.shape)
+
+    # Calculate the new shape based on the resampling factor
+    new_shape = shape * factor
+
+    # Create a new affine matrix
+    new_affine = affine.copy()
+    new_affine[:3, :3] = affine[:3, :3] * (1 / factor)
+
+    # Create a new NIfTI image
+    new_image = nib.Nifti1Image(image_data, affine)
+
+    resampled_image = resample_from_to(new_image, (new_shape, new_affine), order=order)
+    return resampled_image
--- src/segmentor/utils/plotting.py
+++ src/segmentor/utils/plotting.py
@@ -2,6 +2,7 @@ import pyvista as pv
 import numpy as np
 import numpy.typing as npt
 from typing import Any
+from scipy.ndimage import binary_dilation
 
 
 def wrap_numpy_object(obj: npt.NDArray):
@@ -46,3 +47,64 @@ def plot_skeleton_3d(skeleton: Any, volume: npt.NDArray = None):
 
     plotter.view_xz()
     plotter.show()
+
+
+def plotLine3d(x0: int, y0: int, z0: int, x1: int, y1: int, z1: int) -> list[tuple[int, int, int]]:
+    """Based on the Bresenham's line algorithm in 3D.
+    
+    Source: http://members.chello.at/easyfilter/bresenham.html
+
+    Parameters
+    ----------
+    (x0, y0, z0) : tuple of int
+        The start point of the line.
+    (x1, y1, z1) : tuple of int
+        The end point of the line.
+
+    Returns
+    -------
+    list of tuple of int
+        The coordinates of the line
+    """
+    points = []
+    dx = abs(x1 - x0)
+    sx = 1 if x0 < x1 else -1
+    dy = abs(y1 - y0)
+    sy = 1 if y0 < y1 else -1
+    dz = abs(z1 - z0)
+    sz = 1 if z0 < z1 else -1
+    dm = max(dx, dy, dz)
+    i = dm
+    ox1 = oy1 = oz1 = dm // 2
+
+    while True:
+        points.append((x0, y0, z0))
+        if i == 0:
+            break
+        i -= 1
+        ox1 -= dx
+        if ox1 < 0:
+            ox1 += dm
+            x0 += sx
+        oy1 -= dy
+        if oy1 < 0:
+            oy1 += dm
+            y0 += sy
+        oz1 -= dz
+        if oz1 < 0:
+            oz1 += dm
+            z0 += sz
+    return points
+
+
+def path3d(reference: np.ndarray, path: list[tuple], dilate: int = 0) -> np.ndarray:
+    """Draws a path on a 3D volume."""
+    base = np.zeros_like(reference)
+    base[tuple(np.asarray(path).T)] = 1
+    for start, end in zip(path[:-1], path[1:]):
+        base[tuple(np.asarray(plotLine3d(*start, *end)).T)] = 1
+
+    if dilate:
+        base = binary_dilation(base, iterations=dilate)
+
+    return base
